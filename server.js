b74fa676038b01bfd28730aae4d13111ffe916a2
Wrath-Dev <TheWrathDev@gmail.com>
2025-07-26 13:52:58 -0600

    agadf

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const addonInterface = require("./addon")
 serveHTTP(addonInterface, { port: process.env.PORT || 61334 })
 
 // when you've deployed your addon, un-comment this line
-// publishToCentral("https://my-addon.awesome/manifest.json")
+publishToCentral("https://stremio-universal-stream-renamer.onrender.com/manifest.json")
 // for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
